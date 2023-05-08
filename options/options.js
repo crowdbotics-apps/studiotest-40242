a417e91a78491ac2e5b86e90ec0cf526cdc038ae
Nenad Jovanovic <nenad.jovanovic@crowdbotics.com>
2023-05-08 09:59:29 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -2,7 +2,7 @@ export const globalOptions = {
   "name": "studiotest-40242",
   "url": "https://studiotest-40242.botics.co",
   "api": "https://studiotest-40242.botics.co/api/v1",
-  "initialRoute": "Untitled2"
+  "initialRoute": "Untitled4"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
